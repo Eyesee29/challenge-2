224210b5ff55e5ee5aec6067f85ec8e839e54b6f
Eyesee29 <issy.igbinoba@stu.ocsb.ca>
2025-03-18 11:10:54 -0400

    It works!! I made the start sting faster, 'state=x1 is now the leading one'

--- main.ts
+++ main.ts
@@ -3,7 +3,12 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(counter)
 })
 input.onButtonPressed(Button.AB, function () {
-    if (state == x2) {
+    if (state == x1) {
+        x1 = counter
+        basic.showNumber(x1)
+        state = x2
+        counter = 0
+    } else if (state == x2) {
         x2 = counter
         basic.showString("X2")
         basic.showNumber(x2)
@@ -34,19 +39,14 @@ input.onButtonPressed(Button.AB, function () {
         x = randint(0, 100)
         basic.showString("x")
         basic.showNumber(x)
-        b = y1 / (m - x)
-        basic.showNumber(y1 / (m - x))
+        b = y1 - m * x
+        basic.showNumber(y1 - m * x)
         basic.showString("b")
         basic.showNumber(b)
         Y = b + m * x
         basic.showNumber(b + m * x)
         basic.showString("y")
         basic.showNumber(Y)
-    } else {
-        x1 = counter
-        basic.showNumber(x1)
-        state = x2
-        counter = 0
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -57,14 +57,15 @@ let Y = 0
 let b = 0
 let x = 0
 let m = 0
-let x1 = 0
 let y2 = 0
 let y1 = 0
 let x2 = 0
-let state = 0
 let counter = 0
-basic.showString("Yara, Issy & Anastasie")
+let x1 = 0
+let state = 0
+basic.showString("Yara, Issy & Anastasie",100)
 basic.showString("X1?")
+state = x1
 basic.forever(function () {
 	
 })
